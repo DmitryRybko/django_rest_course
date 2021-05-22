37ed9e04d9c58b0ef7fe09accfb373f6f67d3044
Dmitry Rybko <dmr.main@outlook.com>
2021-05-22 23:25:59 +0300

    Дмитрий Рыбко - Homework 03. 1. В проекте создано новое приложение для работы с TODO - todoapp. 2. Добавлена модель Project с полями name и user (manu-to-many к модели CustomUser). 3. Добавлена модель ToDoList с набором полей и связью one-to-may с моделью Project. 4. Создан API для моделей Projects и ToDoList. Использованы ViewSets по аналогии с моделью CustomUser.

--- django_rest_project/urls.py
+++ django_rest_project/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from usersapp.views import UserModelViewSet
+from todoapp.views import ProjectModelViewSet, ToDoListModelViewSet
 
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('todolist', ToDoListModelViewSet)
 
 
 urlpatterns = [
